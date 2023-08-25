fcad2294d3ba805a873e760eb66a2b2f46749b40
katch <79490292+ka-chng@users.noreply.github.com>
2023-08-25 01:29:52 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const { Client } = require('whatsapp-web.js');
-
 const client = new Client();
 
 client.on('qr', (qr) => {
@@ -7,12 +6,20 @@ client.on('qr', (qr) => {
 });
 
 client.on('ready', () => {
-    console.log('Client is ready!');
+    console.log('Client is online');
 });
 
-client.on('message', msg => {
-    if (msg.body == '!ping') {
-        msg.reply('pong');
+client.on('message', message => {
+    if (message.body.toLowerCase() === 'hello') {
+        message.reply('Hello there!');
+    } else if (message.body.toLowerCase() === 'how are you') {
+        message.reply('I am a bot, I do not have feelings, but thank you for asking');
+    } else if (message.body.toLowerCase() === 'what is your name') {
+        message.reply('I am a bot created by your programmer friend');
+    } else if (message.body.toLowerCase().includes('thank you')) {
+        message.reply('You are welcome!');
+    } else {
+        message.reply('Sorry, I did not understand that. Could you please rephrase?');
     }
 });
 
